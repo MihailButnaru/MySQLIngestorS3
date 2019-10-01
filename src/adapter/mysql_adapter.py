ff75f7b0867e8c58650de51c9be208183fb9b4dc
Mihail Butnaru <butnarum@hotmail.com>
2019-10-01 21:55:37 +0100

    Implemented: MysqlAdapter

--- src/adapter/mysql_adapter.py
+++ src/adapter/mysql_adapter.py
@@ -1,2 +1,28 @@
 # Copyright | 2019 | All rights reserved
 # MIHAIL BUTNARU
+from src.config import Config
+from contextlib import contextmanager
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+mysql_engine = create_engine(Config().DATABASE_CONNECTION)
+
+# Session class created configured
+Session = sessionmaker(bind=mysql_engine)
+
+Base = declarative_base()
+
+@contextmanager
+def session_scope():
+    """Provides a transactional scope around a series of operations. """
+    session = Scope()
+    Base.metadata.create_all(mysql_engine) # creates all the tables
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
